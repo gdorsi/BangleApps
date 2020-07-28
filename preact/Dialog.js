47638682e306ad9bbc10a954f02159716ed30dd0
Guido D'Orsi <guido.dorsi@immobiliare.it>
2020-07-28 14:03:29 +0200

    moved Dialog & friends in a separate file

---
+++ preact/Dialog.js
@@ -0,0 +1,168 @@
+import { h } from "https://cdn.skypack.dev/preact";
+import { createPortal } from "https://cdn.skypack.dev/preact/compat";
+import {
+  useState,
+  useEffect,
+  useRef,
+} from "https://cdn.skypack.dev/preact/hooks";
+import htm from "https://cdn.skypack.dev/htm";
+
+const html = htm.bind(h);
+
+function getFocusableElements(el) {
+    return el.querySelectorAll(
+      'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]'
+    );
+  }
+
+export function Dialog({ header, body, footer, onClose }) {
+    const container = document.getElementById("modals");
+  
+    function handleClose(evt) {
+      evt.preventDefault();
+      onClose();
+    }
+  
+    useEffect(() => {
+      const upHandler = ({ code, which }) => {
+        if (code === "Escape" || which === 27) {
+          onClose();
+        }
+      };
+  
+      addEventListener("keyup", upHandler);
+  
+      return () => {
+        removeEventListener("keyup", upHandler);
+      };
+    }, [onClose]);
+  
+    const ref = useRef();
+  
+    useEffect(() => {
+      const focusedElBeforeOpen = document.activeElement;
+  
+      const focusableElements = getFocusableElements(
+        ref.current.querySelector(".modal-container")
+      );
+  
+      //The close button is focused only when it is the only focusable element
+      const focusedEl = focusableElements[1] || focusableElements[0];
+  
+      focusedEl.focus();
+  
+      return () => {
+        focusedElBeforeOpen && focusedElBeforeOpen.focus();
+      };
+    }, []);
+  
+    useEffect(() => {
+      const focusTrap = (evt) => {
+        const { code, which, shiftKey } = evt;
+  
+        if (code !== "Tab" && which !== 9) return;
+  
+        const focusableElements = getFocusableElements(
+          ref.current.querySelector(".modal-container")
+        );
+  
+        if (focusableElements.length === 1) {
+          evt.preventDefault();
+          return;
+        }
+  
+        const first = focusableElements[0];
+        const last = focusableElements[focusableElements.length - 1];
+  
+        if (shiftKey) {
+          if (document.activeElement === first) {
+            evt.preventDefault();
+            last.focus();
+          }
+        } else {
+          if (document.activeElement === last) {
+            evt.preventDefault();
+            first.focus();
+          }
+        }
+      };
+  
+      addEventListener("keydown", focusTrap);
+  
+      return () => {
+        removeEventListener("keydown", focusTrap);
+      };
+    }, []);
+  
+    return createPortal(
+      html`
+        <div role="dialog" class="modal active" ref=${ref}>
+          <a
+            href="#close"
+            class="modal-overlay"
+            aria-label="Close"
+            onClick=${handleClose}
+          ></a>
+          <div class="modal-container">
+            <div class="modal-header">
+              <a
+                href="#close"
+                class="btn btn-clear float-right"
+                aria-label="Close"
+                onClick=${handleClose}
+              ></a>
+              ${header}
+            </div>
+            <div class="modal-body">
+              ${body}
+            </div>
+            ${footer &&
+            html`
+              <div class="modal-footer">
+                ${footer}
+              </div>
+            `}
+          </div>
+        </div>
+      `,
+      container
+    );
+  }
+  
+  export function Confirm({ header, body, onConfirm, onClose }) {
+    return html`
+      <${Dialog}
+        header=${header}
+        body=${body}
+        onClose=${onClose}
+        footer=${html`
+          <button class="btn btn-primary" onClick=${onConfirm}>Yes</button>
+          <button class="btn" onClick=${onClose}>No</button>
+        `}
+      />
+    `;
+  }
+  
+  export function usePrompt(onConfirm) {
+    const [isOpen, setOpen] = useState(false);
+  
+    function show() {
+      setOpen(true);
+    }
+  
+    function handleConfirm(...args) {
+      onConfirm && onConfirm(...args);
+      setOpen(false);
+    }
+  
+    function handleClose() {
+      setOpen(false);
+    }
+  
+    return {
+      show,
+      onConfirm: handleConfirm,
+      onClose: handleClose,
+      isOpen,
+    };
+  }
--- preact/index.js
+++ preact/index.js
@@ -15,6 +15,11 @@ import {
   useAtomValue,
   useSetAtomState,
 } from "./atoms.js";
+import {
+  Dialog,
+  Confirm,
+  usePrompt,
+} from "./Dialog.js";
 
 const html = htm.bind(h);
 
@@ -26,12 +31,6 @@ function getAppGithubURL(app) {
   if (githubMatch) username = githubMatch[1];
 
   return `https://github.com/${username}/BangleApps/tree/master/apps/${app.id}`;
-}
-
-function getFocusableElements(el) {
-  return el.querySelectorAll(
-    'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]'
-  );
 }
 
 /** EFFECTS */
@@ -46,158 +45,6 @@ function getEmulatorURL(app) {
   let url = baseurl + "/apps/" + app.id + "/" + file.url;
 
   return `/emulator.html?codeurl=${url}`;
-}
-
-function Dialog({ header, body, footer, onClose }) {
-  const container = document.getElementById("modals");
-
-  function handleClose(evt) {
-    evt.preventDefault();
-    onClose();
-  }
-
-  useEffect(() => {
-    const upHandler = ({ code, which }) => {
-      if (code === "Escape" || which === 27) {
-        onClose();
-      }
-    };
-
-    addEventListener("keyup", upHandler);
-
-    return () => {
-      removeEventListener("keyup", upHandler);
-    };
-  }, [onClose]);
-
-  const ref = useRef();
-
-  useEffect(() => {
-    const focusedElBeforeOpen = document.activeElement;
-
-    const focusableElements = getFocusableElements(
-      ref.current.querySelector(".modal-container")
-    );
-
-    //The close button is focused only when it is the only focusable element
-    const focusedEl = focusableElements[1] || focusableElements[0];
-
-    focusedEl.focus();
-
-    return () => {
-      focusedElBeforeOpen && focusedElBeforeOpen.focus();
-    };
-  }, []);
-
-  useEffect(() => {
-    const focusTrap = (evt) => {
-      const { code, which, shiftKey } = evt;
-
-      if (code !== "Tab" && which !== 9) return;
-
-      const focusableElements = getFocusableElements(
-        ref.current.querySelector(".modal-container")
-      );
-
-      if (focusableElements.length === 1) {
-        evt.preventDefault();
-        return;
-      }
-
-      const first = focusableElements[0];
-      const last = focusableElements[focusableElements.length - 1];
-
-      if (shiftKey) {
-        if (document.activeElement === first) {
-          evt.preventDefault();
-          last.focus();
-        }
-      } else {
-        if (document.activeElement === last) {
-          evt.preventDefault();
-          first.focus();
-        }
-      }
-    };
-
-    addEventListener("keydown", focusTrap);
-
-    return () => {
-      removeEventListener("keydown", focusTrap);
-    };
-  }, []);
-
-  return createPortal(
-    html`
-      <div role="dialog" class="modal active" ref=${ref}>
-        <a
-          href="#close"
-          class="modal-overlay"
-          aria-label="Close"
-          onClick=${handleClose}
-        ></a>
-        <div class="modal-container">
-          <div class="modal-header">
-            <a
-              href="#close"
-              class="btn btn-clear float-right"
-              aria-label="Close"
-              onClick=${handleClose}
-            ></a>
-            ${header}
-          </div>
-          <div class="modal-body">
-            ${body}
-          </div>
-          ${footer &&
-          html`
-            <div class="modal-footer">
-              ${footer}
-            </div>
-          `}
-        </div>
-      </div>
-    `,
-    container
-  );
-}
-
-function Prompt({ header, body, onConfirm, onClose }) {
-  return html`
-    <${Dialog}
-      header=${header}
-      body=${body}
-      onClose=${onClose}
-      footer=${html`
-        <button class="btn btn-primary" onClick=${onConfirm}>Yes</button>
-        <button class="btn" onClick=${onClose}>No</button>
-      `}
-    />
-  `;
-}
-
-function usePrompt(onConfirm) {
-  const [isOpen, setOpen] = useState(false);
-
-  function show() {
-    setOpen(true);
-  }
-
-  function handleConfirm(...args) {
-    onConfirm && onConfirm(...args);
-    setOpen(false);
-  }
-
-  function handleClose() {
-    setOpen(false);
-  }
-
-  return {
-    show,
-    onConfirm: handleConfirm,
-    onClose: handleClose,
-    isOpen,
-  };
 }
 
 const toastAtom = createStateAtom();
@@ -1123,7 +970,7 @@ function About() {
       </div>
       ${removeAllPrompt.isOpen &&
       html`
-        <${Prompt}
+        <${Confirm}
           header="Remove all"
           body="Really remove all apps?"
           onConfirm=${removeAllPrompt.onConfirm}
@@ -1132,7 +979,7 @@ function About() {
       `}
       ${installDefaultPrompt.isOpen &&
       html`
-        <${Prompt}
+        <${Confirm}
           header="Install Defaults"
           body="Remove everything and install default apps?"
           onConfirm=${installDefaultPrompt.onConfirm}
